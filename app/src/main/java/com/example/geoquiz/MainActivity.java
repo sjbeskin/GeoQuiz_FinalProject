c6aba8ff289833bea40df1a8ef7c222428a03d00
Stefan Beskin <stefanbeskin@gmail.com>
2021-02-23 14:39:24 -0800

    Questions can now only be answered once and the percentage of correct answers is displayed at the end.

--- app/src/main/java/com/example/geoquiz/MainActivity.java
+++ app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -3,14 +3,20 @@ package com.example.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -18,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton mBackButton;
     private TextView mTextNextButton;
     private TextView mQuestionTextView;
+    private int mNumCorrect = 0;
 
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_australia, true),
@@ -28,12 +35,19 @@ public class MainActivity extends AppCompatActivity {
             new Question(R.string.question_asia, true),
     };
 
+    private ArrayList<Integer> mQuestionsAnswered = new ArrayList<Integer>();
+
     private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(bundle) called");
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
@@ -41,14 +55,20 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mQuestionsAnswered.add(mCurrentIndex);
                 checkAnswer(true);
+                mTrueButton.setEnabled(false);
+                mFalseButton.setEnabled(false);
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
            public void onClick(View v) {
+               mQuestionsAnswered.add(mCurrentIndex);
                checkAnswer(false);
+               mTrueButton.setEnabled(false);
+               mFalseButton.setEnabled(false);
            }
         });
 
@@ -57,6 +77,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                if(mQuestionsAnswered.contains(mCurrentIndex)) {
+                    mTrueButton.setEnabled(false);
+                    mFalseButton.setEnabled(false);
+                } else {
+                    mTrueButton.setEnabled(true);
+                    mFalseButton.setEnabled(true);
+                }
                 updateQuestion();
             }
         });
@@ -68,6 +95,13 @@ public class MainActivity extends AppCompatActivity {
                mCurrentIndex = (mCurrentIndex - 1);
                if(mCurrentIndex < 0){
                    mCurrentIndex = mQuestionBank.length - 1;
+               }
+               if(mQuestionsAnswered.contains(mCurrentIndex)) {
+                   mTrueButton.setEnabled(false);
+                   mFalseButton.setEnabled(false);
+               } else {
+                   mTrueButton.setEnabled(true);
+                   mFalseButton.setEnabled(true);
                }
                updateQuestion();
             }
@@ -85,6 +119,43 @@ public class MainActivity extends AppCompatActivity {
         updateQuestion();
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
@@ -97,6 +168,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (userPressedTrue == answerIsTrue) {
             messageResId = R.string.correct_toast;
+            mNumCorrect++;
         } else {
             messageResId = R.string.incorrect_toast;
         }
@@ -104,5 +176,13 @@ public class MainActivity extends AppCompatActivity {
         Toast myToast = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT);
         myToast.setGravity(Gravity.TOP, 0, 0);
         myToast.show();
+
+        if(mQuestionsAnswered.size() == mQuestionBank.length){
+            int score = (int) Math.round((((mNumCorrect + 0.0) / mQuestionBank.length) * 100));
+            String scoreText = "Score: " + score + "%";
+            Toast scoreToast = Toast.makeText( this, scoreText, Toast.LENGTH_LONG);
+            scoreToast.setGravity(Gravity.CENTER, 0, -250);
+            scoreToast.show();
+        }
     }
 }
